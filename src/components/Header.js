c4a19eb4daa931fc45cee493c6654715ba858968
Aditi <aditijaiswal275@gmail.com>
2022-07-01 00:01:37 +0530

    Adding logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react"
-
+import reactLogo from "../images/logo.png"
 
 const Header = () => {
     return (
         <div>
             <nav className="nav">
-                <h1>React</h1>
+                <img src={reactLogo} alt="React-logo" className="nav--icon" />
+                <h1 className="heading">React</h1>
                 <ul className="nav-items">
                     <li>Pricing</li>
                     <li>About</li>
